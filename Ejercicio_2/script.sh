23a267140353d994b3ba6bef0e623137b16a8ba5
Claudio Morales <climoralesg@gmail.com>
2025-07-28 23:17:12 -0400

    main: Ejercicio 2

---
+++ Ejercicio_2/script.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#2. Variables y Operaciones
+#Crea un script que defina dos variables, las sume y muestre el resultado.
+
+variableUno=1
+variableDos=1
+
+sum=$(($variableUno + $variableDos))
+
+echo "$sum"
