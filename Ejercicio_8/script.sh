2f336a21883836990e263c839d013868f0489df1
Claudio Morales <climoralesg@gmail.com>
2025-08-05 20:49:17 -0400

    main: Ejercicio 8

---
+++ Ejercicio_8/script.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#8. Leer Entrada del Usuario
+#Crea un script que pida al usuario su nombre y luego imprima un saludo personalizado.
+
+name=""
+
+read -p "Ingrese su nombre: " name
+
+echo -e "Hola $name\n"
+
